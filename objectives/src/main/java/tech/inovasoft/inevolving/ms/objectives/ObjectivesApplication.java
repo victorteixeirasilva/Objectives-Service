804f1f13f425aca1f1ef0fdbbb35677034e3af91
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-05-15 12:01:21 -0300

    feat: Iniciando Projeto Spring Boot.

---
+++ objectives/src/main/java/tech/inovasoft/inevolving/ms/objectives/ObjectivesApplication.java
@@ -0,0 +1,13 @@
+package tech.inovasoft.inevolving.ms.objectives;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectivesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObjectivesApplication.class, args);
+	}
+
+}
