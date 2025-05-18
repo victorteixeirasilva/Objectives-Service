b5ee1b85109c8bccf9f549ebfad0e3e8b30df011
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-05-18 13:22:00 -0300

    feat: Desenvolver Teste Que Falhe. (save).

---
+++ objectives/src/test/java/tech/inovasoft/inevolving/ms/objectives/unit/success/ObjectiveRepositoryImplementationSuccessTest.java
@@ -0,0 +1,48 @@
+package tech.inovasoft.inevolving.ms.objectives.unit.success;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import tech.inovasoft.inevolving.ms.objectives.domain.dto.request.RequestCreateObjectiveDTO;
+import tech.inovasoft.inevolving.ms.objectives.domain.model.Objective;
+import tech.inovasoft.inevolving.ms.objectives.repository.implementation.ObjectiveRepositoryImplementation;
+import tech.inovasoft.inevolving.ms.objectives.repository.interfaces.ObjectiveJpaRepository;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class ObjectiveRepositoryImplementationSuccessTest {
+
+    @Mock
+    private ObjectiveJpaRepository objectiveJpaRepository;
+
+    @InjectMocks
+    private ObjectiveRepositoryImplementation objectiveRepositoryImplementation;
+
+    @Test
+    public void save() {
+        //Given
+        RequestCreateObjectiveDTO request = new RequestCreateObjectiveDTO(
+                "Objetivo 1",
+                "Descrição 1",
+                UUID.randomUUID()
+        );
+
+        Objective expected = new Objective(request);
+        expected.setId(UUID.randomUUID());
+
+        //When
+        when(objectiveJpaRepository.save(expected)).thenReturn(expected);
+        var result = objectiveRepositoryImplementation.save(expected);
+
+        //Then
+        assertNotNull(result);
+        verify(objectiveJpaRepository).save(expected);
+    }
+}
