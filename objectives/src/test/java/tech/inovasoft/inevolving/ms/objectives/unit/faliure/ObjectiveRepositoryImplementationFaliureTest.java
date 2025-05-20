4830e6861be6201587070f8299ef811facfeaa59
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-05-19 22:48:04 -0300

    feat: finalizando camada de teste de falha da classe ObjectiveRepositoryImplementation

---
+++ objectives/src/test/java/tech/inovasoft/inevolving/ms/objectives/unit/faliure/ObjectiveRepositoryImplementationFaliureTest.java
@@ -0,0 +1,165 @@
+package tech.inovasoft.inevolving.ms.objectives.unit.faliure;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import tech.inovasoft.inevolving.ms.objectives.domain.dto.request.RequestCreateObjectiveDTO;
+import tech.inovasoft.inevolving.ms.objectives.domain.exception.DataBaseException;
+import tech.inovasoft.inevolving.ms.objectives.domain.exception.NotFoundObjectivesByUser;
+import tech.inovasoft.inevolving.ms.objectives.domain.model.Objective;
+import tech.inovasoft.inevolving.ms.objectives.domain.model.Status;
+import tech.inovasoft.inevolving.ms.objectives.repository.implementation.ObjectiveRepositoryImplementation;
+import tech.inovasoft.inevolving.ms.objectives.repository.interfaces.ObjectiveJpaRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class ObjectiveRepositoryImplementationFaliureTest {
+
+    @Mock
+    private ObjectiveJpaRepository objectiveJpaRepository;
+
+    @InjectMocks
+    private ObjectiveRepositoryImplementation objectiveRepositoryImplementation;
+
+    @Test
+    public void saveDataBaseException() throws DataBaseException {
+        //Given
+        RequestCreateObjectiveDTO request = new RequestCreateObjectiveDTO(
+                "Objetivo 1",
+                "Descrição 1",
+                UUID.randomUUID()
+        );
+
+        Objective expected = new Objective(request);
+        expected.setId(UUID.randomUUID());
+
+        //When
+        when(objectiveJpaRepository.save(expected)).thenThrow(new RuntimeException());
+        var result = assertThrows(DataBaseException.class, () -> {
+            objectiveRepositoryImplementation.save(expected);
+        });
+
+        //Then
+        assertNotNull(result);
+        assertEquals("Database error: (save)", result.getMessage());
+
+        verify(objectiveJpaRepository).save(expected);
+    }
+
+    @Test
+    public void findByIdAndIdUserDataBaseException() throws DataBaseException, NotFoundObjectivesByUser {
+        //Given
+        var idObjective = UUID.randomUUID();
+        var idUser = UUID.randomUUID();
+        var objective = new Objective();
+        objective.setId(idObjective);
+        objective.setNameObjective("Objetivo");
+        objective.setDescriptionObjective("Objetivo");
+        objective.setIdUser(idUser);
+
+        //When
+        when(objectiveJpaRepository.findByIdAndIdUser(idObjective, idUser)).thenThrow(new RuntimeException());
+
+        var result = assertThrows(DataBaseException.class, () -> {
+            objectiveRepositoryImplementation.findByIdAndIdUser(idObjective, idUser);
+        });
+
+        //Then
+        assertNotNull(result);
+        assertEquals("Database error: (findByIdAndIdUser)", result.getMessage());
+
+        verify(objectiveJpaRepository, times(1)).findByIdAndIdUser(idObjective, idUser);
+    }
+
+    @Test
+    public void findByIdAndIdUserNotFoundObjectivesByUser() throws DataBaseException, NotFoundObjectivesByUser {
+        //Given
+        var idObjective = UUID.randomUUID();
+        var idUser = UUID.randomUUID();
+        var objective = new Objective();
+        objective.setId(idObjective);
+        objective.setNameObjective("Objetivo");
+        objective.setDescriptionObjective("Objetivo");
+        objective.setIdUser(idUser);
+
+        //When
+        when(objectiveJpaRepository.findByIdAndIdUser(idObjective, idUser)).thenReturn(Optional.empty());
+
+        var result = assertThrows(NotFoundObjectivesByUser.class, () -> {
+            objectiveRepositoryImplementation.findByIdAndIdUser(idObjective, idUser);
+        });
+
+        //Then
+        assertNotNull(result);
+        assertEquals("Not found objectives by user!", result.getMessage());
+
+        verify(objectiveJpaRepository, times(1)).findByIdAndIdUser(idObjective, idUser);
+    }
+
+    @Test
+    public void findAllByIdUser() throws DataBaseException {
+        //Given
+        var idUser = UUID.randomUUID();
+        List<Objective> objectives = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            objectives.add(new Objective(
+                    UUID.randomUUID(),
+                    "Objetivo " + i,
+                    "Descrição " + i,
+                    Status.TODO,
+                    null,
+                    idUser
+            ));
+        }
+
+        //When
+        when(objectiveJpaRepository.findAllByIdUser(idUser)).thenThrow(new RuntimeException());
+        var result = assertThrows(DataBaseException.class, () -> {
+            objectiveRepositoryImplementation.findAllByIdUser(idUser);
+        });
+
+        //Then
+        assertNotNull(result);
+        assertEquals("Database error: (findAllByIdUser)", result.getMessage());
+
+        verify(objectiveJpaRepository, times(1)).findAllByIdUser(idUser);
+    }
+
+    @Test
+    public void findAllByIdUserAndStatus() throws DataBaseException {
+        //Given
+        var idUser = UUID.randomUUID();
+        List<Objective> objectives = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            objectives.add(new Objective(
+                    UUID.randomUUID(),
+                    "Objetivo " + i,
+                    "Descrição " + i,
+                    Status.TODO,
+                    null,
+                    idUser
+            ));
+        }
+
+        //When
+        when(objectiveJpaRepository.findAllByIdUserAndStatus(idUser, Status.TODO)).thenThrow(new RuntimeException());
+        var result = assertThrows(DataBaseException.class, () -> {
+            objectiveRepositoryImplementation.findAllByIdUserAndStatus(idUser, Status.TODO);
+        });
+
+        //Then
+        assertNotNull(result);
+        assertEquals("Database error: (findAllByIdUserAndStatus)", result.getMessage());
+
+        verify(objectiveJpaRepository, times(1)).findAllByIdUserAndStatus(idUser, Status.TODO);
+    }
+}
