beefd363b4222aadbca377c6e6dbdc4aecb17f81
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-06-10 19:51:19 -0300

    Desenvolvendo teste de integração, TaskServiceClientTest

--- objectives/src/main/java/tech/inovasoft/inevolving/ms/objectives/service/client/TasksServiceClient.java
+++ objectives/src/main/java/tech/inovasoft/inevolving/ms/objectives/service/client/TasksServiceClient.java
@@ -5,19 +5,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import tech.inovasoft.inevolving.ms.objectives.domain.dto.response.ResponseMessageDTO;
 
 import java.sql.Date;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
 @Service
-@FeignClient(name = "tasks-service", url = "http://localhost:8081/ms/tasks") //TODO: Mudar para o endereço do container e esconder ele em variaveis de ambiente
+@FeignClient(name = "tasks-service", url = "http://localhost:8081/ms/tasks")
 public interface TasksServiceClient {
 
-    // TODO: Fazer teste de integração.
     @DeleteMapping("/lock/{completionDate}/{idUser}/{idObjective}")
-    ResponseEntity lockTaskByObjective(
-            // TODO: Desenvolver DTO dos dados recebidos, e preencher o ResponseEntity
+    ResponseEntity<ResponseMessageDTO> lockTaskByObjective(
             @PathVariable Date completionDate,
             @PathVariable UUID idUser,
             @PathVariable UUID idObjective
--- objectives/src/test/java/tech/inovasoft/inevolving/ms/objectives/integration/TaskServiceClientTest.java
+++ objectives/src/test/java/tech/inovasoft/inevolving/ms/objectives/integration/TaskServiceClientTest.java
@@ -1,11 +1,36 @@
 package tech.inovasoft.inevolving.ms.objectives.integration;
 
+import io.restassured.http.ContentType;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class TaskServiceClientTest {
 
-    //TODO: Desenvolver teste de integração
+    @Test
+    public void integrationTest_Ok() {
+
+        RequestSpecification requestSpecification = given()
+                .contentType(ContentType.JSON);
+
+        ValidatableResponse response = requestSpecification
+                .when()
+                .get("http://localhost:8085/actuator/health")
+                .then();
+
+        response.assertThat().statusCode(200).and()
+                .body("status", equalTo("UP"));
+
+    }
 
 }
